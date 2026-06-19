f12d83a85eee3aeb0862774dcb9e8ecae1774981
sagarsahu271994 <sagarsahu271994@gmail.com>
2026-06-19 15:27:53 +0530

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: { formats: ["image/avif", "image/webp"] },
-  poweredByHeader: false,
-  experimental: { optimizePackageImports: ["lucide-react"] }
-};
-
-export default nextConfig;
