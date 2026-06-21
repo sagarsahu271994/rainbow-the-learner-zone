8cac23fb1f6522861fbf103dd249f48d0d4732a6
sagarsahu271994 <sagarsahu271994@gmail.com>
2026-06-21 19:46:35 +0530

    page.tsx

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Admin Dashboard
+      </h1>
+
+      <p className="mt-4">
+        Welcome to Rainbow Admin Panel
+      </p>
+    </div>
+  );
+}
