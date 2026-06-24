7d1f635e427b48a2f33cd62dfe4651d079fd95a2
sagarsahu271994 <sagarsahu271994@gmail.com>
2026-06-24 22:34:53 +0530

    Update route.ts

--- app/api/fees/route.ts
+++ app/api/fees/route.ts
@@ -1,80 +1,79 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(
+req: Request
+) {
+
 try {
 
-```
 const body =
-  await req.json();
+await req.json();
 
 const SCRIPT_URL =
-  process.env
-    .NEXT_PUBLIC_FEES_SCRIPT_URL;
+process.env
+.NEXT_PUBLIC_FEES_SCRIPT_URL;
 
-if (!SCRIPT_URL) {
-  return NextResponse.json(
-    {
-      success:false,
-      error:
-        "Fees webhook missing",
-    },
-    {
-      status:500,
-    }
-  );
+if(
+!SCRIPT_URL
+){
+
+return NextResponse.json(
+{
+success:false
+},
+{
+status:500
+}
+);
+
 }
 
 const response =
-  await fetch(
-    SCRIPT_URL,
-    {
-      method:"POST",
+await fetch(
+SCRIPT_URL,
+{
+method:"POST",
 
-      headers:{
-        "Content-Type":
-          "application/json",
-      },
+headers:{
+"Content-Type":
+"application/json"
+},
 
-      body:
-        JSON.stringify(
-          body
-        ),
-    }
-  );
+body:
+JSON.stringify(
+body
+)
+}
+);
 
-const result =
-  await response.json();
+const data =
+await response.text();
 
 return NextResponse.json({
-  success:true,
 
-  pdfUrl:
-    result.pdfUrl || "",
+success:true,
 
-  data:
-    result,
+pdfUrl:data
+
 });
-```
 
-} catch (error) {
+}
 
-```
-console.error(
-  error
+catch(error){
+
+console.log(
+error
 );
 
 return NextResponse.json(
-  {
-    success:false,
-
-    error:
-      "Fees Save Failed",
-  },
-  {
-    status:500,
-  }
+{
+success:false
+},
+{
+status:500
+}
 );
-```
 
 }
+
 }
