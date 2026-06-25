02ee7c1c981904cfb6e402bb59b5b44ed3d28364
sagarsahu271994 <sagarsahu271994@gmail.com>
2026-06-25 15:08:06 +0530

    Update route.ts

--- app/api/fees/route.ts
+++ app/api/fees/route.ts
@@ -1,73 +1,88 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(
+req: Request
+) {
+
 try {
+
 const body =
 await req.json();
-  const receiptNumber =
-body.receiptNo
-||
-`RTL-${1250}`;
+
+const receiptNumber =
+body.receiptNo ||
+"RTL-1250";
+
 const SCRIPT_URL =
-  process.env
-    .NEXT_PUBLIC_FEES_SCRIPT_URL;
+process.env
+.NEXT_PUBLIC_FEES_SCRIPT_URL;
 
 if (!SCRIPT_URL) {
-  return NextResponse.json(
-    {
-      success: false,
-      error: "Missing Script URL",
-    },
-    {
-      status: 500,
-    }
-  );
+
+return NextResponse.json(
+{
+success:false,
+error:
+"Missing Script URL",
+},
+{
+status:500,
+}
+);
+
 }
 
 const response =
-  await fetch(
-    SCRIPT_URL,
-    {
-      method: "POST",
+await fetch(
+SCRIPT_URL,
+{
+method:"POST",
 
-      headers: {
-        "Content-Type":
-          "application/json",
-      },
+headers:{
+"Content-Type":
+"application/json",
+},
 
-    body:
+body:
 JSON.stringify({
 ...body,
 receiptNo:
-receiptNumber
-})
-        ),
-  );
+receiptNumber,
+}),
 
-const result =
-  await response.text();
-
-return NextResponse.json({
-  success: true,
-  pdfUrl: result,
-});
-
-} catch (error) {
-
-console.error(
-  error
+}
 );
 
+const result =
+await response.text();
+
 return NextResponse.json(
-  {
-    success: false,
-    error:
-      "Save Failed",
-  },
-  {
-    status: 500,
-  }
+{
+success:true,
+pdfUrl:
+result,
+}
 );
 
 }
+
+catch(error){
+
+console.error(
+error
+);
+
+return NextResponse.json(
+{
+success:false,
+error:
+"Save Failed",
+},
+{
+status:500,
+}
+);
+
+}
+
 }
