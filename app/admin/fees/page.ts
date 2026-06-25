933ac6ea3d0589e6223a5216080115f69bdf7298
sagarsahu271994 <sagarsahu271994@gmail.com>
2026-06-25 21:01:54 +0530

    Update page.ts

--- app/admin/fees/page.ts
+++ app/admin/fees/page.ts
@@ -1,1 +1,60 @@
+import { NextResponse } from "next/server";
 
+export async function POST(req: Request) {
+  try {
+
+    const body =
+      await req.json();
+
+    const SCRIPT_URL =
+      process.env
+        .NEXT_PUBLIC_FEES_SCRIPT_URL;
+
+    if (!SCRIPT_URL) {
+      return NextResponse.json(
+        {
+          success: false,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    const response =
+      await fetch(
+        SCRIPT_URL,
+        {
+          method: "POST",
+
+          headers: {
+            "Content-Type":
+              "application/json",
+          },
+
+          body:
+            JSON.stringify(body),
+        }
+      );
+
+    const result =
+      await response.text();
+
+    return NextResponse.json({
+      success: true,
+      pdfUrl: result,
+    });
+
+  } catch {
+
+    return NextResponse.json(
+      {
+        success: false,
+      },
+      {
+        status: 500,
+      }
+    );
+
+  }
+}
